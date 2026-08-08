39360c8db8f9ee447b53164c71b31f4d06fbfdbf
Haardik Sagar <haardiksagar@gmail.com>
2026-08-08 17:25:30 +0530

    Add REST controller for order book state

---
+++ web/src/main/java/com/ironbook/matching_engine/web/controller/OrderBookController.java
@@ -0,0 +1,24 @@
+package com.ironbook.matching_engine.web.controller;
+
+import com.ironbook.matching_engine.MatchingEngine;
+import com.ironbook.matching_engine.Model.Snapshot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+public class OrderBookController {
+
+    private final MatchingEngine matchingEngine;
+
+    public OrderBookController(MatchingEngine matchingEngine) {
+        this.matchingEngine = matchingEngine;
+    }
+
+    @GetMapping
+    public Snapshot getOrderBookSnapshot(@RequestParam(defaultValue = "20") int levels) {
+        return matchingEngine.getSnapshot(levels);
+    }
+}
